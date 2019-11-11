f8fe38a45e58be8b883a911e00bab53d9baedef8
illindima <illindima@gmail.com>
2019-11-11 18:29:31 +0200

    display user category per scale; add icons;

--- src/components/Scale/Scale.js
+++ src/components/Scale/Scale.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Paper, Typography, Grid, Box } from '@material-ui/core';
 import BookmarkIcon from '@material-ui/icons/Bookmark';
+import CheckIcon from '@material-ui/icons/Check';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(() => ({
@@ -21,12 +22,32 @@ function Scale(props) {
 	const { scale } = props;
 	const classes = useStyles();
 
+	const userScaleCategories = scale.categories.filter(category => {
+		return (
+			scale.value >= category.range.low && scale.value < category.range.high
+		);
+	});
+
 	return (
 		<Paper square={true} className={classes.root}>
 			<Typography variant="h5" className={classes.title}>
 				{scale.scaleTitle}
 			</Typography>
-			<Typography variant="subtitle1">Ваш результат: {scale.value}</Typography>
+			<Box className={classes.categoryBox}>
+				<CheckIcon />
+				<Typography variant="subtitle1">
+					Ваш результат: {scale.value}
+				</Typography>
+			</Box>
+			{!!userScaleCategories.length && (
+				<Box className={classes.categoryBox}>
+					<CheckIcon />
+					<Typography variant="subtitle1">
+						Категорія:
+						{userScaleCategories.map(scaleCateogy => scaleCateogy.title)}
+					</Typography>
+				</Box>
+			)}
 
 			<Grid
 				container
@@ -41,13 +62,13 @@ function Scale(props) {
 								<BookmarkIcon />
 								<Typography variant="h6">{category.title}</Typography>
 							</Box>
-							<Typography variant="body1">{category.description}</Typography>
 							<Typography variant="body2">
 								Мінімальне значення категорії: {category.range.low}
 							</Typography>
 							<Typography variant="body2">
 								Максимальне значення категорії: {category.range.high}
 							</Typography>
+							<Typography variant="body1">{category.description}</Typography>
 						</Grid>
 					);
 				})}
--- src/components/ScaleList/ScaleList.js
+++ src/components/ScaleList/ScaleList.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import Scale from '../Scale';
 import { Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
